90dc8f526a0a04281b00b0fea4188cd049ea2927
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-08-24 15:13:32 +0000

    :clap: :clap: :clap:

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,64 @@
+#![allow(irrefutable_let_patterns)]
+
 pub mod parser;
 pub mod token;
 pub mod cursor;
 
-use std::{env::args, path::PathBuf, fs};
+use std::{path::PathBuf, fs, io::stdout, process::exit};
 
+use clap::{Parser as ClapParser, Subcommand, Command, CommandFactory, error::ErrorKind};
+use clap_complete::{Shell, Generator, generate};
 use parser::Parser;
 use tokio::main;
 use serde_json::to_string_pretty;
 
+#[derive(ClapParser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// The path to the file to compile.
+    file: Option<String>,
+
+    /// A sub-command.
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Generates shell completions.
+    Completion {
+        /// The shell to generate for.
+        shell: Shell,
+    },
+}
+
+fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
+    generate(gen, cmd, cmd.get_name().to_string(), &mut stdout());
+}
+
 #[main]
 pub async fn main() {
-    let path = args().nth(1).unwrap();
+    let cli = Cli::parse();
+
+    if let Some(command) = cli.command {
+        if let Commands::Completion { shell } = command {
+            let mut cmd = Cli::command();
+
+            print_completions(shell, &mut cmd);
+
+            return;
+        }
+    }
+
+    if cli.file.is_none() {
+        let mut cmd = Cli::command();
+        let err = cmd.error(ErrorKind::MissingRequiredArgument, "Missing value for file!");
+
+        err.print().unwrap();
+        exit(1);
+    }
+
+    let path = cli.file.unwrap();
     let path = PathBuf::from(path);
     let content = fs::read_to_string(path).unwrap();
     let tokens = Parser::new(content).parse();
